5911f1bcfcf1b38d0878c1bab5ddd3aba46487a3
savini-roza <52928720+savini-roza@users.noreply.github.com>
2020-05-20 21:26:10 -0300

    Atividade6

---
+++ Ex6.js
@@ -0,0 +1,12 @@
+var aluno1 = {
+	RA: 1234567890,
+	Nome: "Ednaldo Pereira"
+};
+
+var aluno2 = new Object();
+aluno2.RA = 2245687654;
+aluno2.Nome = "Jailson Mendes";
+
+var aluno3 = {};
+aluno3["RA"] = 876544563;
+aluno3["Nome"] = "Marselo";
